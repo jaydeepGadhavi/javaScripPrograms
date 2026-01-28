5e210fbd2146761d71b76df2af5bc60bc4164226
Jaydeep Gadhavi <jaydeepbgadhavi@gmail.com>
2026-01-28 13:09:22 +0530

    [JD] [variable & Datatypes files are added]

---
+++ datatypes.js
@@ -0,0 +1,137 @@
+// two types of data types
+// 1. primitve
+    // Number
+    // BigInt
+    // String
+    // boolean
+    // Symbol
+    // null
+    // undefined
+
+// 2. Non primitive
+    // Array
+    // Object
+    // function
+
+// let num = 1;
+// let num2 = 2;
+
+// console.log("num",num, num2);
+
+// let name = "prince";
+// let name2 = "Jaydeep"
+
+// console.log("names :", name2, name)
+
+// Boolean
+
+// let isTrue = true;
+// let isFalse = false;
+
+// console.log("boolean :", isTrue)
+
+// undefined
+
+// let a;
+// console.log("undefined :", a)
+
+
+// let BigInt = 1234567806543233n;
+// console.log(BigInt);
+
+// 2. Non primitive
+// object
+
+// let Student = {
+//     name : "prince",
+//     roll : 65,
+//     address:{
+//         city:"jnd",
+//     }
+// }
+// console.log(Student);
+
+// Array
+
+// // let a = [1,2,3,4,5,6,7,8,9];
+// // let student = {
+
+// // }
+// // let arrayOfStud = [{},{}]
+
+
+// // console.log(a);
+
+
+// // Function
+
+// function greet() {
+//     console.log("this is function");
+// }
+// greet();
+
+// function showValues(value){
+//     console.log("this is value :",value);
+//     if(value == 20){
+//         console.log("number-----");
+//     }
+// }
+// showValues();
+// showValues(20);
+// showValues([1,2,3,4,5]);
+// showValues();
+
+// const add = (num1, num2) => {
+//     return num1 + num2
+// }
+// const mul = (num1, num2) =>{
+//     return num1 * num2
+// }
+// const sub = (num1, num2) =>{
+//     return num1 - num2
+// }
+// const div = (num1, num2) =>{
+//     return num1 / num2
+// }
+// console.log('this is arrow fun. for add : ' , add(5,5))
+// console.log('this is arrow fun. for mul : ' , mul(5,5))
+// console.log('this is arrow fun. for sub : ' , sub(5,5))
+// console.log('this is arrow fun. for div: ' , div(5,5))
+
+// let value = 10;
+// console.log('DEFAULT VALUE : ', value);
+
+// value += 5 // value + 5
+// console.log(value)
+
+// value -= 5 // value - 5
+// console.log(value)
+
+// value *= 5 // value * 5
+// console.log(value)
+
+// value /= 5 // value / 5
+// console.log(value)
+
+// let age = 20;
+// let hasId = false
+// let name = "students"
+// let obj = {
+//     name: 'example',
+//     rollNo: 22
+// }
+// console.log("logical AND (&&) : ", age > 18 && hasId);
+// console.log("logical OR (||) : ", age < 18 || hasId)
+// console.log("logical NOT (!) : ",  !hasId)
+
+// console.log(typeof age)
+// console.log(typeof hasId)
+// console.log(typeof name)
+// console.log(typeof obj)
+
+let value = 10;
+let num = "10"
+
+console.log("value == num : ", value == num );
+console.log("value === num : ", value === num);
+console.log("value != num : ", value != num)
---
+++ variable.js
@@ -0,0 +1,18 @@
+// VAR
+var a = 10;
+
+var a = 20;
+console.log(a);
+
+// let
+let a = 20;
+
+if (true) {
+    let a = 10;
+}
+console.log(a);
+
+// const
+
+const pi = 3.14;
+console.log(pi);
